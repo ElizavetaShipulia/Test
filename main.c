6eab990b9c502f18f2faa2784f8c6d38789b123e
ElizavetaShipulia <liza-shp@mail.ru>
2025-04-20 23:50:57 +0300

    main.c

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include "i2c_rgbw.h"
 
 int main(void) {
-    RGBW_Init(); 
-
+    RGBW_Init(); // Инициализация I2C и микросхемы управления светодиодами
+    // Включаем все каналы:
     RGBW_AllOnOff(1);
 
     RGBW_SetColor(255, 128, 0, 0); 
-
+    // Установливаем разную яркость каналов
     RGBW_SetBrightness(0, 128); 
     RGBW_SetBrightness(1, 64);  
     RGBW_SetBrightness(2, 192); 
     RGBW_SetBrightness(3, 255); 
-
+    // Выключить только красный (R)
     RGBW_ChannelOnOff(0, 0);
-
+    // Установливаем одну яркость для всех
     RGBW_SetAllBrightness(100);
 
     while (1) {
